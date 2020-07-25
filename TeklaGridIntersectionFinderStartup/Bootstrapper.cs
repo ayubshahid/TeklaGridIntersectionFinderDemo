ec8693a0b0d4a5a063c70ddd360a97e493e09523
Muhammad Ayub <m.ayub@LIVE.IT>
2020-07-25 23:10:42 +0100

    Add project files.

---
+++ TeklaGridIntersectionFinderStartup/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using TeklaGridIntersectionFinderStartup.ViewModels;
+
+namespace TeklaGridIntersectionFinderStartup
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ TeklaGridIntersectionFinderStartup/ViewModels/ShellViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using System.Collections.ObjectModel;
+
+namespace TeklaGridIntersectionFinderStartup.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        private ObservableCollection<string> _levels = new ObservableCollection<string> { "0.00"};
+        public ObservableCollection<string> Levels
+        {
+            get { return _levels; }
+            set { _levels = value; NotifyOfPropertyChange(() => Levels); }
+        }
+
+        private string _currentLevel;
+        public string CurrentLevel
+        {
+            get { return _currentLevel; }
+            set { _currentLevel = value; NotifyOfPropertyChange(() => CurrentLevel); }
+        }
+    }
+}
